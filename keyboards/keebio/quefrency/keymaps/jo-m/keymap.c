323ffb1187b2111db1917964bd7dd2a5a0c55225
Jonathan Müller <jo-m@outlook.com>
2020-07-07 23:36:25 +0200

    macros

--- keyboards/keebio/quefrency/keymaps/jo-m/keymap.c
+++ keyboards/keebio/quefrency/keymaps/jo-m/keymap.c
@@ -19,7 +19,9 @@ void matrix_init_user(void) {
 
 // unicode
 enum unicode_names {
-    ae,AE,oe,OE,ue,UE,
+    // umlauts
+    ae, AE, oe, OE, ue, UE,
+    // emojis
     THUU, EXCL, FIST, MAHN, LOL,
     SCRM, HMMM, CRY, BOX
 };
@@ -42,14 +44,53 @@ const uint32_t PROGMEM unicode_map[] = {
     [BOX]   = 0x1F94A, // 🥊
 };
 
+
+// macros
+enum custom_macro_keycodes {
+    GIT_ADD_P = SAFE_RANGE,
+    GIT_COM_M,
+    GIT_PUSH,
+    GIT_PULL,
+    CD_DOWN,
+    LS_LA,
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (!record->event.pressed) {
+        return true;
+    }
+    switch (keycode) {
+    case GIT_ADD_P:
+        SEND_STRING("git add -p");
+        break;
+    case GIT_COM_M:
+        SEND_STRING("git commit -m ''");
+        tap_code(KC_LEFT);
+        break;
+    case GIT_PUSH:
+        SEND_STRING("git push");
+        break;
+    case GIT_PULL:
+        SEND_STRING("git pull");
+        break;
+    case CD_DOWN:
+        SEND_STRING("cd ..\n");
+        break;
+    case LS_LA:
+        SEND_STRING("ls -lah\n");
+        break;
+    }
+    return true;
+};
+
 // keymaps
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_65_with_macro(
     KC_VOLD,  KC_VOLU,   KC_GESC,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   _______,  KC_BSPC,  KC_HOME,  \
     KC_BRID,  KC_BRIU,   KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_END,  \
     KC_MUTE,  KC_PSCREEN,KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,   KC_PGUP,  \
-    RESET,    _______,   KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,    KC_PGDN,  \
-    _______,  _______,   MO(_FN1), KC_LCTL,  KC_LGUI,  KC_LALT,  KC_SPC,             KC_SPC,   KC_RALT,  KC_RGUI,  KC_RCTL,  MO(_FN1), KC_LEFT,  KC_DOWN,  KC_RGHT
+    GIT_ADD_P,GIT_COM_M, KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,    KC_PGDN,  \
+    GIT_PUSH, GIT_PULL,  MO(_FN1), KC_LCTL,  KC_LGUI,  KC_LALT,  KC_SPC,             KC_SPC,   KC_RALT,  KC_RGUI,  KC_RCTL,  MO(_FN1), KC_LEFT,  KC_DOWN,  KC_RGHT
   ),
 
   [_FN1] = LAYOUT_65_with_macro(
@@ -57,6 +98,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,  _______,   _______,  _______,  _______,   X(EXCL),  X(LOL),   X(THUU),  _______,  XP(ue,UE),_______,  XP(oe,OE),_______,  _______, KC_GRAVE,  _______,  _______,  \
     _______,  _______,   _______,  XP(ae,AE),X(SCRM),  _______,  X(FIST),  _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  _______,  _______,  _______,  _______,  \
     _______,  _______,   _______,  X(MAHN),  _______,  X(CRY),   _______,  X(BOX),   _______,  X(HMMM),  _______,  _______,  _______,  _______,  KC_PGUP,  _______,  \
-    _______,  _______,   _______,  _______,  _______,  _______,  _______,            _______,  _______,  _______,  _______,  _______,  KC_HOME,  KC_RGHT,  KC_END
+    CD_DOWN,  LS_LA,     _______,  _______,  _______,  _______,  _______,            _______,  _______,  _______,  _______,  _______,  KC_HOME,  KC_RGHT,  KC_END
   )
 };
