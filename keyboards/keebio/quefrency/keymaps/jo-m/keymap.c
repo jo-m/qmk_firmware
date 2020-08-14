b6be0392e3e852462a984b272de6f5d91a17d3e2
Jonathan Müller <jo-m@outlook.com>
2020-08-15 01:16:48 +0200

    remove unicode and unused macros

--- keyboards/keebio/quefrency/keymaps/jo-m/keymap.c
+++ keyboards/keebio/quefrency/keymaps/jo-m/keymap.c
@@ -40,15 +40,6 @@ const uint32_t PROGMEM unicode_map[] = {
     [OE]    =  0x00D6,  // Ö
     [ue]    =  0x00FC,  // ü
     [UE]    =  0x00DC,  // Ü
-    [THUU]  = 0x1F44D, // 👍
-    [EXCL]  = 0x1F44C, // 👌
-    [FIST]  = 0x1F44A, // 👊
-    [MAHN]  = 0x1F446, // 👆
-    [LOL]   = 0x1F602, // 😂
-    [SCRM]  = 0x1F631, // 😱
-    [HMMM]  = 0x1F9D0, // 🧐
-    [CRY]   = 0x1F62D, // 😭
-    [BOX]   = 0x1F94A, // 🥊
 };
 
 
@@ -81,15 +72,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case GIT_PULL:
         SEND_STRING("git pull");
         break;
-    case CD_DOWN:
-        SEND_STRING("cd ..\n");
-        break;
-    case CD_BACK:
-        SEND_STRING("cd -\n");
-        break;
-    case LS_LA:
-        SEND_STRING("ls -lah\n");
-        break;
     }
     return true;
 };
@@ -101,14 +83,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_BRID,  KC_BRIU,   KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_END,  \
     KC_MUTE,  KC_PSCREEN,KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,   KC_PGUP,  \
     GIT_ADD_P,GIT_COM_M, KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,    KC_PGDN,  \
-    GIT_PUSH, GIT_PULL,  MO(_FN1), KC_LCTL,  KC_LGUI,  KC_LALT,  KC_SPC,             KC_SPC,   KC_RALT,  KC_RGUI,  KC_RCTL,  MO(_FN1), KC_LEFT,  KC_DOWN,  KC_RGHT
+    GIT_PULL, GIT_PUSH,  MO(_FN1), KC_LCTL,  KC_LGUI,  KC_LALT,  KC_SPC,             KC_SPC,   KC_RALT,  KC_RGUI,  KC_RCTL,  MO(_FN1), KC_LEFT,  KC_DOWN,  KC_RGHT
   ),
 
   [_FN1] = LAYOUT_65_with_macro(
     _______,  _______,   _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,  KC_DEL,  _______,  \
-    _______,  _______,   _______,  _______,  _______,  X(EXCL),  X(LOL),   X(THUU),  _______,  XP(ue,UE),_______,  XP(oe,OE),_______,  _______, KC_GRAVE,  _______,  _______,  \
-    _______,  _______,   _______,  XP(ae,AE),X(SCRM),  _______,  X(FIST),  _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  _______,  _______,  _______,  _______,  \
-    CD_BACK,  _______,   _______,  X(MAHN),  _______,  X(CRY),   _______,  X(BOX),   _______,  X(HMMM),  _______,  _______,  _______,  _______,  KC_PGUP,  _______,  \
-    CD_DOWN,  LS_LA,     _______,  _______,  _______,  _______,  _______,            _______,  _______,  _______,  _______,  _______,  KC_HOME,  KC_PGDN,  KC_END
+    _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  XP(ue,UE),_______,  XP(oe,OE),_______,  _______, KC_GRAVE,  _______,  _______,  \
+    _______,  _______,   _______,  XP(ae,AE),_______,  _______,  _______,  _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  _______,  _______,  _______,  _______,  \
+    _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_PGUP,  _______,  \
+    _______,  _______,   _______,  _______,  _______,  _______,  _______,            _______,  _______,  _______,  _______,  _______,  KC_HOME,  KC_PGDN,  KC_END
   )
 };
