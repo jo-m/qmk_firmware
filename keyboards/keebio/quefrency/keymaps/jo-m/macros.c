81aa21c1c3791e2c16d46d1a2914a69085e4c6c6
Jonathan Müller <jo-m@outlook.com>
2021-01-24 21:40:40 +0100

    update rev1 with common stuff

--- keyboards/keebio/quefrency/keymaps/jo-m/macros.c
+++ keyboards/keebio/quefrency/keymaps/jo-m/macros.c
@@ -1,13 +1,31 @@
 enum custom_macro_keycodes {
+    // git add -p
     GIT_ADD_P = SAFE_RANGE,
+    // git commit -m '' <-
     GIT_COM_M,
+    // git push
     GIT_PUSH,
+    // git pull
     GIT_PULL,
+    // tmux zoom
     TMX_ZOOM,
+    // tmux create window
     TMX_CRTE,
+
+    // mic push to talk
+    KC_MPTT,
 };
 
+// KC_F20 is AudioMicMute (https://github.com/linuxmint/gtk/blob/master/gdk/keynames.txt)
+#define KC_MMUTE KC_F20
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    // mic push to talk
+    if (keycode == KC_MPTT) {
+        tap_code(KC_F20);
+        return true;
+    }
+
     if (!record->event.pressed) {
         return true;
     }
