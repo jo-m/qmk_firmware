48367a3ee49e6b44dd6c6c5814f87d5498b79979
Jonathan Müller <jo-m@outlook.com>
2021-01-27 19:00:36 +0100

    make prtsc use ctrl by default, use scroll wheel for encoder, change rgb key layout, add soft reset and eeprom reset

--- keyboards/keebio/quefrency/keymaps/jo-m-rev2/keymap.c
+++ keyboards/keebio/quefrency/keymaps/jo-m-rev2/keymap.c
@@ -11,8 +11,8 @@
 // keymaps
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_65_with_macro(
-    _______,  MO(_LED),  KC_GESC,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   _______,  KC_BSPC,  _______,  \
-    KC_MMUTE, KC_MPTT,   KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_PSCR,  \
+    KC_MUTE,  MO(_LED),  KC_GESC,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   _______,  KC_BSPC, _______,  \
+    KC_MMUTE, KC_MPTT,   KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  C(KC_PSCR),  \
     KC_MUTE,  KC_MPLY,   KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,   KC_PGUP,  \
     GIT_ADD_P,GIT_COM_M, KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,    KC_PGDN,  \
     GIT_PULL, GIT_PUSH,  MO(_FN1), KC_LCTL,  KC_LGUI,  KC_LALT,  KC_SPC,             KC_SPC,   KC_RALT,  KC_RGUI,  KC_RCTL,  MO(_FN1), KC_LEFT,  KC_DOWN,  KC_RGHT
@@ -28,10 +28,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_LED] = LAYOUT_65_with_macro(
     _______,  _______,   RGB_M_P,  RGB_M_B,  RGB_M_R,  RGB_M_SW, RGB_M_SN, RGB_M_K,  RGB_M_G,  _______,  _______,  _______,  _______,  _______, _______,   _______,  _______,  _______,  \
-    BL_STEP,  BL_BRTG,   RGB_TOG,  RGB_RMOD, RGB_MOD,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  \
-    RGB_HUD,  RGB_HUI,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  \
+    RGB_HUD,  RGB_HUI,   RGB_TOG,  RGB_RMOD, RGB_MOD, _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  \
     RGB_SAD,  RGB_SAI,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  \
-    RGB_VAD,  RGB_VAI,   _______,  _______,  _______,  _______,  _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
+    RGB_VAD,  RGB_VAI,   _______,  _______,  _______,  _______,  RESET,    EEP_RST,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  \
+    BL_STEP,  BL_BRTG,   _______,  _______,  _______,  _______,  _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
   )
 };
 
@@ -52,9 +52,9 @@ void encoder_update_user(uint8_t index, bool clockwise) {
   // right encoder
   else if (index == _RIGHT) {
     if (clockwise) {
-        tap_code(KC_PGUP);
+        tap_code(KC_WH_D);
     } else {
-        tap_code(KC_PGDN);
+        tap_code(KC_WH_U);
     }
   }
 }
