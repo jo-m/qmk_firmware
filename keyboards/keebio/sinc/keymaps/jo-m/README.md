b81fd54163d5ba449a22693e0df2546d5691a9b8
Jonathan Müller <jo-m@outlook.com>
2022-06-18 13:56:14 +0200

    fix sinc

--- keyboards/keebio/sinc/keymaps/jo-m/README.md
+++ keyboards/keebio/sinc/keymaps/jo-m/README.md
@@ -7,8 +7,18 @@ qmk new-keymap -kb keebio/sinc/rev1 -km jo-m
 Deploy:
 
 ```bash
-cd repo_root_folder
-make keebio/sinc/rev1:jo-m:flash
+cd qmk_firmware
+
+# prepare
+make setup
+make git-submodule
+
+# build
+qmk clean -a
+qmk compile -kb keebio/sinc/rev1 -km jo-m
+
+# flash
+qmk flash -kb keebio/sinc/rev1 -km jo-m
 ```
 
 Upgrade from upstream:
--- keyboards/keebio/sinc/keymaps/jo-m/keymap.c
+++ keyboards/keebio/sinc/keymaps/jo-m/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 };
 
-void encoder_update_user(uint8_t index, bool clockwise) {
+bool encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
         if (clockwise) {
             tap_code(KC_VOLU);
@@ -58,4 +58,6 @@ void encoder_update_user(uint8_t index, bool clockwise) {
             tap_code(KC_MPRV);
         }
     }
+
+    return false;
 }
